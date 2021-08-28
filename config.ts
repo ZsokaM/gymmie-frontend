7a5d9fa47366a0884df3b1e506291532475e7dad
ZsokaM <zsokamontvai@gmail.com>
2021-08-28 15:15:04 +0200

    url

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-//export const endpoint = `https://gymmie-backend-hgkt5.ondigitalocean.app/`
-export const endpoint = `http://localhost:3000/api/graphql`
+export const endpoint = `https://gymmie-backend-hgkt5.ondigitalocean.app/`
+//export const endpoint = `http://localhost:3000/api/graphql`
 export const prodEndpoint = `fill me in when we deploy`
