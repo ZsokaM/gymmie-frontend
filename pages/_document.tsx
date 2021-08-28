811f030aa72213b3ba89c419aa120297c95a7ab7
ZsokaM <zsokamontvai@gmail.com>
2021-08-28 15:40:03 +0200

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ import Document, {
 } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument {
+export default class MyDocument extends Document {
   static getInitialProps({ renderPage }: any) {
     const sheet = new ServerStyleSheet()
     const page = renderPage((App: any) => (props: any) =>
